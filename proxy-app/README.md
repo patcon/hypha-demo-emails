bf63217aa96f0bce280377a14ed6ee3f387d0f2c
Patrick Connolly <patrick.c.connolly@gmail.com>
2019-11-27 13:30:08 -0500

    Added small argo proxy app.

---
+++ proxy-app/README.md
@@ -0,0 +1,7 @@
+# Demo Mailcow Auth Proxy
+
+The purpose of this app is to allow a custom restricted token to only
+allow a small subset of actions within the MailCow API.
+
+Specifically, this proxy currently only allows listing, editing, and
+creating of aliases.
---
+++ proxy-app/server.js
@@ -0,0 +1,22 @@
+var argo = require('argo');
+
+var BASE_URL = 'https://mailninja.aseriesoftubez.com';
+var MAILCOW_TOKEN_FULL = process.env.MAILCOW_TOKEN_FULL || 'xxxxxxxxxxxx';
+var HYPHA_TOKEN_RESTRICTED = process.env.HYPHA_TOKEN_RESTRICTED || '1234567890';
+
+var restricted_token = function(handle) {
+  handle('request', function(env, next) {
+    if (env.request.headers['x-api-key-hypha'] == HYPHA_TOKEN_RESTRICTED) {
+      env.request.headers['x-api-key'] = MAILCOW_TOKEN_FULL;
+    }
+    next(env);
+  })
+}
+
+argo()
+  .target(BASE_URL)
+  // list
+  .get('^/api/v1/get/alias/(all|\\d+)', restricted_token)
+  // add or edit
+  .post('^/api/v1/(edit|add)/alias', restricted_token)
+  .listen(process.env.PORT || 1337)
