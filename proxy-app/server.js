0fb4e2a907cf9ecb04e597d6f1331bff75768adf
Patrick Connolly <patrick.c.connolly@gmail.com>
2019-12-09 19:26:17 -0500

    Oops. Bugfix.

--- proxy-app/server.js
+++ proxy-app/server.js
@@ -7,12 +7,12 @@ var HYPHA_TOKEN_RESTRICTED = process.env.HYPHA_TOKEN_RESTRICTED || '1234567890';
 var restricted_token = function(handle) {
   handle('request', function(env, next) {
     if (env.request.headers['x-api-key'] == HYPHA_TOKEN_RESTRICTED) {
-      console.log("Successful X-API-Key header for restricted access. Passing through with full token.";
+      console.log("Successful X-API-Key header for restricted access. Passing through with full token.");
       env.request.headers['x-api-key'] = MAILCOW_TOKEN_FULL;
     } else {
-      console.log("Incorrect or missing X-API-Key header. Passing through without special access.";
+      console.log("Incorrect or missing X-API-Key header. Passing through without special access.");
     }
-    console.log("Proxying request to " + BASE_URL;);
+    console.log("Proxying request to " + BASE_URL);
     next(env);
   })
 }
